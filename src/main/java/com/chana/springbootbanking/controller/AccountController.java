7ba8a6699dbd56f92134f27cf715677a4cfb0c7b
Chanvitha Praveen <chanvithapraween@gmail.com>
2024-05-08 09:15:21 +0530

    created the getAccountById API

--- src/main/java/com/chana/springbootbanking/controller/AccountController.java
+++ src/main/java/com/chana/springbootbanking/controller/AccountController.java
@@ -4,10 +4,7 @@ import com.chana.springbootbanking.dto.AccountDto;
 import com.chana.springbootbanking.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/accounts")
 @RestController
@@ -22,5 +19,11 @@ public class AccountController {
     @PostMapping()
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto) {
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
+        AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
     }
 }
